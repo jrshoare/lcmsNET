06d789e2c516e27f627b43d2c35ae65781d857a5
jrshoare <john.stevensonhoare@gmail.com>
2019-09-06 15:21:09 +0100

    Re-factored DeltaE class to use read-only references to avoid copying value types.

--- src/lcmsNET/DeltaE.cs
+++ src/lcmsNET/DeltaE.cs
@@ -22,7 +22,7 @@
             return Interop.CIE94DeltaE(lab1, lab2);
         }
 
-        public static double CIEDE2000(CIELab lab1, CIELab lab2, double kL = 1.0, double kC = 1.0, double kH = 1.0)
+        public static double CIEDE2000(in CIELab lab1, in CIELab lab2, double kL = 1.0, double kC = 1.0, double kH = 1.0)
         {
             return Interop.CIE2000DeltaE(lab1, lab2, kL, kC, kH);
         }
