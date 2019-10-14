3caeb7f13725c282eaa836c2b1d3667877f85564
jrshoare <john.stevensonhoare@gmail.com>
2019-10-14 16:18:58 +0100

    Added methods to create and free IT8 objects.

---
+++ src/lcmsNET/IT8.cs
@@ -0,0 +1,65 @@
+﻿using lcmsNET.Impl;
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace lcmsNET
+{
+    public sealed class IT8 : IDisposable
+    {
+        private IntPtr _handle;
+
+        internal IT8(IntPtr handle, Context context = null)
+        {
+            Helper.CheckCreated<IT8>(handle);
+
+            _handle = handle;
+            Context = context;
+        }
+
+        public static IT8 Create(Context context)
+        {
+            return new IT8(Interop.IT8Alloc(context?.Handle ?? IntPtr.Zero), context);
+        }
+
+        #region Properties
+        public Context Context { get; private set; }
+        #endregion
+
+        #region IDisposable Support
+        public bool IsDisposed => _handle == IntPtr.Zero;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void EnsureNotDisposed()
+        {
+            if (_handle == IntPtr.Zero)
+            {
+                throw new ObjectDisposedException(nameof(IT8));
+            }
+        }
+
+        private void Dispose(bool disposing)
+        {
+            var handle = Interlocked.Exchange(ref _handle, IntPtr.Zero);
+            if (handle != IntPtr.Zero)
+            {
+                Interop.IT8Free(handle);
+                Context = null;
+            }
+        }
+
+        ~IT8()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+
+        internal IntPtr Handle => _handle;
+    }
+}
---
+++ src/lcmsNET/Interop/Interop.IT8.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace lcmsNET
+{
+    internal static partial class Interop
+    {
+        [DllImport(Liblcms, EntryPoint = "cmsIT8Alloc", CallingConvention = CallingConvention.StdCall)]
+        private static extern IntPtr IT8Alloc_Internal(
+            IntPtr contextID);
+
+        internal static IntPtr IT8Alloc(IntPtr contextID)
+        {
+            return IT8Alloc_Internal(contextID);
+        }
+
+        [DllImport(Liblcms, EntryPoint = "cmsIT8Free", CallingConvention = CallingConvention.StdCall)]
+        private static extern void IT8Free_Internal(IntPtr handle);
+
+        internal static void IT8Free(IntPtr handle)
+        {
+            IT8Free_Internal(handle);
+        }
+    }
+}
