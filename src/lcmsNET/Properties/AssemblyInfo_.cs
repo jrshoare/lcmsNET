9e09a4969bd88a23248d0a8efbdbc1fa12794453
jrshoare <john.stevensonhoare@gmail.com>
2022-02-11 09:01:27 +0000

    Make internals visible to unit testing.

---
+++ src/lcmsNET/Properties/AssemblyInfo_.cs
@@ -0,0 +1,29 @@
+﻿// Copyright(c) 2019-2022 John Stevenson-Hoare
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo(
+    "lcmsNET.Tests, PublicKey=" +
+    "0024000004800000940000000602000000240000525341310004000001000100d960582b05cee1" +
+    "bc64da7741441015f18d29f8de00c26e7fdbbdd8efe302c96224bb6462aa1c38c712329ad143eb" +
+    "fe4b0f2920d5040351ea112bf8cdce240ebe1876171d7745163f9d717c3f48d29b465a8657edbe" +
+    "bfac1e6ecab3563c5e0e8f90943e6dd13adcc4f7c88fd887c02b2e3f8f9d24f3d021fb3a3d121b" +
+    "e4d38fd7")]
