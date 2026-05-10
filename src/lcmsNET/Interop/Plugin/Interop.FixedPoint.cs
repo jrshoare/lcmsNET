f652344adbd54344fd9d2e9bdf8f94d21c4ca754
jrshoare <john.stevensonhoare@gmail.com>
2026-05-10 15:15:45 +0100

    Support LibraryImport for .NET 7+ interop methods
    Add conditional compilation to use LibraryImport and UnmanagedCallConv for P/Invoke methods on .NET 7.0 or greater, while retaining DllImport for earlier versions. This improves compatibility and performance for fixed-point and double conversion interop functions.

--- src/lcmsNET/Interop/Plugin/Interop.FixedPoint.cs
+++ src/lcmsNET/Interop/Plugin/Interop.FixedPoint.cs
@@ -24,36 +24,64 @@ namespace lcmsNET
 {
     internal static partial class Interop
     {
+#if NET7_0_OR_GREATER
+        [LibraryImport(Liblcms, EntryPoint = "_cms8Fixed8toDouble")]
+        [UnmanagedCallConv(CallConvs = new System.Type[] { typeof(System.Runtime.CompilerServices.CallConvStdcall) })]
+        private static partial double Fixed8Dot8ToDouble_Internal(
+                [MarshalAs(UnmanagedType.U2)] ushort fixed8);
+#else
         [DllImport(Liblcms, EntryPoint = "_cms8Fixed8toDouble", CallingConvention = CallingConvention.StdCall)]
         private static extern double Fixed8Dot8ToDouble_Internal(
                 [MarshalAs(UnmanagedType.U2)] ushort fixed8);
+#endif
 
         internal static double Fixed8Dot8ToDouble(ushort fixed8)
         {
             return Fixed8Dot8ToDouble_Internal(fixed8);
         }
 
+#if NET7_0_OR_GREATER
+        [LibraryImport(Liblcms, EntryPoint = "_cmsDoubleTo8Fixed8")]
+        [UnmanagedCallConv(CallConvs = new System.Type[] { typeof(System.Runtime.CompilerServices.CallConvStdcall) })]
+        private static partial ushort DoubleToFixed8Dot8_Internal(
+                [MarshalAs(UnmanagedType.R8)] double val);
+#else
         [DllImport(Liblcms, EntryPoint = "_cmsDoubleTo8Fixed8", CallingConvention = CallingConvention.StdCall)]
         private static extern ushort DoubleToFixed8Dot8_Internal(
                 [MarshalAs(UnmanagedType.R8)] double val);
+#endif
 
         internal static ushort DoubleToFixed8Dot8(double val)
         {
             return DoubleToFixed8Dot8_Internal(val);
         }
 
+#if NET7_0_OR_GREATER
+        [LibraryImport(Liblcms, EntryPoint = "_cms15Fixed16toDouble")]
+        [UnmanagedCallConv(CallConvs = new System.Type[] { typeof(System.Runtime.CompilerServices.CallConvStdcall) })]
+        private static partial double Fixed15Dot16ToDouble_Internal(
+                [MarshalAs(UnmanagedType.I4)] int fixed32);
+#else
         [DllImport(Liblcms, EntryPoint = "_cms15Fixed16toDouble", CallingConvention = CallingConvention.StdCall)]
         private static extern double Fixed15Dot16ToDouble_Internal(
                 [MarshalAs(UnmanagedType.I4)] int fixed32);
+#endif
 
         internal static double Fixed15Dot16ToDouble(int fixed32)
         {
             return Fixed15Dot16ToDouble_Internal(fixed32);
         }
 
+#if NET7_0_OR_GREATER
+        [LibraryImport(Liblcms, EntryPoint = "_cmsDoubleTo15Fixed16")]
+        [UnmanagedCallConv(CallConvs = new System.Type[] { typeof(System.Runtime.CompilerServices.CallConvStdcall) })]
+        private static partial int DoubleToFixed15Dot16_Internal(
+                [MarshalAs(UnmanagedType.R8)] double val);
+#else
         [DllImport(Liblcms, EntryPoint = "_cmsDoubleTo15Fixed16", CallingConvention = CallingConvention.StdCall)]
         private static extern int DoubleToFixed15Dot16_Internal(
                 [MarshalAs(UnmanagedType.R8)] double val);
+#endif
 
         internal static int DoubleToFixed15Dot16(double val)
         {
