06a6c11b1b2e3175c1b9556c54946e2e113b1bb7
jrshoare <john.stevensonhoare@gmail.com>
2026-05-14 14:00:18 +0100

    Add LibraryImport interop for .NET 7+ Dict functions
    Updated interop binding to [LibraryImport] and [UnmanagesCallConv] for .NET 7+ for Interop Dict static classes, with conditional compilation for legacy support.

--- src/lcmsNET/Interop/Interop.Dict.cs
+++ src/lcmsNET/Interop/Interop.Dict.cs
@@ -25,32 +25,62 @@ namespace lcmsNET
 {
     internal static partial class Interop
     {
+#if NET7_0_OR_GREATER
+        [LibraryImport(Liblcms, EntryPoint = "cmsDictAlloc")]
+        [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvStdcall) })]
+        private static partial IntPtr DictAlloc_Internal(
+                IntPtr contextID);
+#else
         [DllImport(Liblcms, EntryPoint = "cmsDictAlloc", CallingConvention = CallingConvention.StdCall)]
         private static extern IntPtr DictAlloc_Internal(
                 IntPtr contextID);
+#endif
 
         internal static IntPtr DictAlloc(IntPtr contextID)
         {
             return DictAlloc_Internal(contextID);
         }
 
+#if NET7_0_OR_GREATER
+        [LibraryImport(Liblcms, EntryPoint = "cmsDictFree")]
+        [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvStdcall) })]
+        private static partial void DictFree_Internal(IntPtr handle);
+#else
         [DllImport(Liblcms, EntryPoint = "cmsDictFree", CallingConvention = CallingConvention.StdCall)]
         private static extern void DictFree_Internal(IntPtr handle);
+#endif
 
         internal static void DictFree(IntPtr handle)
         {
             DictFree_Internal(handle);
         }
 
+#if NET7_0_OR_GREATER
+        [LibraryImport(Liblcms, EntryPoint = "cmsDictDup")]
+        [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvStdcall) })]
+        private static partial IntPtr DictDup_Internal(
+                IntPtr handle);
+#else
         [DllImport(Liblcms, EntryPoint = "cmsDictDup", CallingConvention = CallingConvention.StdCall)]
         private static extern IntPtr DictDup_Internal(
                 IntPtr handle);
+#endif
 
         internal static IntPtr DictDup(IntPtr handle)
         {
             return DictDup_Internal(handle);
         }
 
+#if NET7_0_OR_GREATER
+        [LibraryImport(Liblcms, EntryPoint = "cmsDictAddEntry")]
+        [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvStdcall) })]
+        private static partial int DictAddEntry_Internal(
+                IntPtr handle,
+                [MarshalAs(UnmanagedType.LPWStr)] string name,
+                [MarshalAs(UnmanagedType.LPWStr)] string value,
+                IntPtr displayName,
+                IntPtr displayValue);
+#else
         [DllImport(Liblcms, EntryPoint = "cmsDictAddEntry", CallingConvention = CallingConvention.StdCall)]
         private static extern int DictAddEntry_Internal(
                 IntPtr handle,
@@ -58,24 +88,39 @@ namespace lcmsNET
                 [MarshalAs(UnmanagedType.LPWStr)] string value,
                 IntPtr displayName,
                 IntPtr displayValue);
+#endif
 
         internal static int DictAddEntry(IntPtr handle, string name, string value, IntPtr displayName, IntPtr displayValue)
         {
             return DictAddEntry_Internal(handle, name, value, displayName, displayValue);
         }
 
+#if NET7_0_OR_GREATER
+        [LibraryImport(Liblcms, EntryPoint = "cmsDictGetEntryList")]
+        [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvStdcall) })]
+        private static partial IntPtr DictGetEntryList_Internal(
+                IntPtr handle);
+#else
         [DllImport(Liblcms, EntryPoint = "cmsDictGetEntryList", CallingConvention = CallingConvention.StdCall)]
         private static extern IntPtr DictGetEntryList_Internal(
                 IntPtr handle);
+#endif
 
         internal static IntPtr DictGetEntryList(IntPtr handle)
         {
             return DictGetEntryList_Internal(handle);
         }
 
+#if NET7_0_OR_GREATER
+        [LibraryImport(Liblcms, EntryPoint = "cmsDictNextEntry")]
+        [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvStdcall) })]
+        private static partial IntPtr DictNextEntry_Internal(
+                IntPtr handle);
+#else
         [DllImport(Liblcms, EntryPoint = "cmsDictNextEntry", CallingConvention = CallingConvention.StdCall)]
         private static extern IntPtr DictNextEntry_Internal(
                 IntPtr handle);
+#endif
 
         internal static IntPtr DictNextEntry(IntPtr handle)
         {
